4db7d3aee51883bef6cd8d0f15ad34bb359c56f5
Xavi Mañá <xavier.manya@gmail.com>
2022-02-19 15:55:54 +0100

    build set up

---
+++ test/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(name='xops',
+      version='0.0.1',
+      description='A test to learn to package',
+      long_description='A test to learn to package',
+      url='https://github.com/Xavi-Linux/Collarbone/tree/main/test',
+      author='xavi-linux',
+      author_email='xavi.ms@xavims.net',
+      keywords='xops',
+      packages=find_packages(include=['xops']))
