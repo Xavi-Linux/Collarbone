f95862ba7311957d675ad0937142a949c84f2b2b
Xavi Mañá <xavier.manya@gmail.com>
2022-02-19 16:21:16 +0100

    ancillary

--- test/setup.py
+++ test/setup.py
@@ -4,8 +4,9 @@ setup(name='xops',
       version='0.0.1',
       description='A test to learn to package',
       long_description='A test to learn to package',
-      url='https://github.com/Xavi-Linux/Collarbone/tree/main/test',
+      url='https://github.com/Xavi-Linux/Collarbone/tree/main/test/xops',
       author='xavi-linux',
       author_email='xavi.ms@xavims.net',
       keywords='xops',
-      packages=find_packages(include=['xops']))
+      packages=find_packages(include=['xops']),
+      python_requires='>=3.6, <4')
